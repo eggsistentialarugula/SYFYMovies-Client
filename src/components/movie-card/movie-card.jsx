edd0c00466972c4202fa48bf05bc7703d58f2a84
Jeannie <jntsao@gmail.com>
2021-07-17 07:14:59 -0700

    use PropTypes to validate data types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
 export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
 
-        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+        return (
+            <div onClick={() => onMovieClick(movie)} className="movie-card">{movie.Title}</div>
+        );
     }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired
+        }),
+        Filmstars: PropTypes.arrayOf(
+            PropTypes.oneOfType([
+                PropTypes.string.isRequired
+            ])
+        ),
+        ReleaseYear: PropTypes.number,
+        IMDbRating: PropTypes.string.isRequired,
+        Image: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
