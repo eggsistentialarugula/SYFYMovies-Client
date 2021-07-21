9e3a0304ea5cbd008eadc970b5152762b1a21e2a
Jeannie <jntsao@gmail.com>
2021-07-21 00:54:21 -0700

    fixed line spacing

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 export class MovieView extends React.Component {
     render() {
         const { movie, onBackClick } = this.props;
