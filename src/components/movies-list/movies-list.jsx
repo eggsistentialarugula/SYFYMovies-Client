5583bae5f14dd19aa807998b3fbfbf76ee99fa47
Jeannie <jntsao@gmail.com>
2021-08-11 02:01:22 -0700

    made changes to style

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { SplitButton } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/NavDropdown'
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input.jsx';
 import { MovieCard } from '../movie-card/movie-card.jsx';
@@ -19,14 +24,8 @@ function MoviesList(props) {
     return <>
         <Col md={12} style={{ margin: '1em' }}>
             <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+            <hr />
         </Col>
-
-        {filteredMovies.map(movie => (
-            <Col lg={4} md={6} xs={12} key={movie._id}>
-                <MovieCard movie={movie} />
-            </Col>
-        ))}
-
         {filteredMovies.map(movie => (
             <Col lg={4} md={6} xs={12} key={movie._id}>
                 <MovieCard movie={movie} />
