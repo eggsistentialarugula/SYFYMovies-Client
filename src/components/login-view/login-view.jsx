8a79eda83cdd962626debd9eebc99435bc829999
Jeannie <jntsao@gmail.com>
2021-11-04 07:18:58 -0700

    added spinner to login button and put on placeholder to use given login credentials

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -8,6 +8,7 @@ import PropTypes from 'prop-types';
 // React Bootstrap components
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner'
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Figure from 'react-bootstrap/Figure';
@@ -22,6 +23,8 @@ export function LoginView(props) {
     const validated = useState(false);
 
     const handleSubmit = (e) => {
+        const showSpinner = document.getElementById('loginSpinner');
+        showSpinner.hidden = false;
         e.preventDefault();
         /* Send a request to the server for authentication */
         axios.post('https://mysyfymovies.herokuapp.com/login', {
@@ -33,6 +36,7 @@ export function LoginView(props) {
             console.log('login successful', data);
         }).catch(e => {
             console.log('no such user');
+            showSpinner.hidden = true;
             alert('Invalid Username or Password');
         });
     };
@@ -48,19 +52,19 @@ export function LoginView(props) {
                         alt="171x180"
                     src={Logo}
                     /> */}
-                    <h1><span className = "welcome-text">Welcome to </span> <span className = "syfy-title">mySyfy Movies</span> </h1>
+                    <h1><span className="welcome-text">Welcome to </span> <span className="syfy-title">mySyfy Movies</span> </h1>
                     <Form.Text>Please login to continue</Form.Text>
                     <Form.Group controlId="formUsername">
                         <Form.Label> Username: </Form.Label>
-                        <Form.Control type="text" placeholder="Enter your username" value={username} onChange={e => setUsername(e.target.value)} pattern="[A-Za-z0-9]{5,}" required minLength="5" />
+                        <Form.Control type="text" placeholder="Use testuser as a username" value={username} onChange={e => setUsername(e.target.value)} pattern="[A-Za-z0-9]{5,}" required minLength="5" />
                         <Form.Control.Feedback type="invalid">Must contain numbers and/or letters and be at least 5 characters.</Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group controlId="formPassword">
                         <Form.Label> Password: </Form.Label>
-                        <Form.Control type="password" placeholder="Enter your password" value={password} onChange={e => setPassword(e.target.value)} required minLength="5" />
+                        <Form.Control type="password" placeholder="Use testuserpw as a password" value={password} onChange={e => setPassword(e.target.value)} required minLength="5" />
                         <Form.Control.Feedback type="invalid">Must be at least 5 characters.</Form.Control.Feedback>
                     </Form.Group>
-                    <Button className="formButton" type="submit" variant="warning" onClick={handleSubmit}>Login</Button>
+                    <Button className="formButton" type="submit" variant="warning" onClick={handleSubmit}><Spinner className="mx-1" id="loginSpinner" as="span" animation="border" size="sm" role="status" aria-hidden="true" hidden />Login</Button>
                     <span>
                         <Form.Text><h5>Don't have an account?</h5></Form.Text>
                         <Link to={`/register`}>
